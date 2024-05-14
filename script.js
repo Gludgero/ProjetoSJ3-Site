bf16a408abc193f623fec7c79240fae19d1c1aa5
Gludgero <guiludgero@sj3.com.br>
2024-05-14 11:07:48 -0300

    Adicionar Navbar e logos

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("nav").style.top = "0";
+    } else {
+      document.getElementById("nav").style.top = "-50px";
+    }
